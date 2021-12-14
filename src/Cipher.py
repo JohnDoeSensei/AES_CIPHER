103d453ccd951e5906db3ad4ce92cad0a23638b0
lpoitevin1 <louis.poitevin@etu.univ-lyon1.fr>
2021-12-14 19:50:05 +0100

    add padding and unpadding

--- src/Cipher.py
+++ src/Cipher.py
@@ -1,48 +1,95 @@
 from Crypto.Cipher import AES
-from Crypto import Random
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
-from binascii import hexlify as hexa
+import hashlib
 
-
+PADD_CHAR = '\\'.encode('utf-8')
 BLOCK_SIZE = 16
 
-class AESCipher:
-    def ask_password(self):
-        password = input('[*] Enter password: ')
-        print('Lenght\'s password: '+str(password))
-        self.ask_password = password
 
-    def __init__(self, type, size):
-        """
-        AES supports multiple key sizes: 16 (AES128), 24 (AES192), or 32 (AES256).
-        """
-        if type == 'AES':
-            self.type = 'AES_CIPHER'
-        # Generate string random bytes
-        if size == 16 or size == 24 or size == 32: 
-            self.key = get_random_bytes(size)
-        self.iv = get_random_bytes(AES.block_size)
+
+class AESCipher:
+    """
+    AES256 cipher in CBC mode. Encrypt and decrypt by the couple (key/iv) with 256 bits keys
+    """
+    
+    def __init__(self):
+        # Padded store
+        self.padded = 0
+        # Generate key hash
+        self.key = self.gen_key()
+        # Generate 16 random bytes for IV
+        self.iv = self.gen_iv()
         # Save key in Cipher object
-        print('[+] Generate key', hexa(self.key).decode('utf-8'))
-        # Create unique cipher
+        print('[+] Generate key', (self.key))
          
 
+    def ask_password(self):
+        """
+        Function to ask password
+        """
+        password = input('[*] Enter password: ')
+        password = password.encode('utf-8')
+        return password
+
+    def gen_key(self):
+        """
+        Generate hashkey in 256 bits
+        """
+        return hashlib.sha256().digest()
+
+    def gen_iv(self):
+        """
+        Generate IV for first block in CBC mode
+        """
+        return get_random_bytes(BLOCK_SIZE)
+
+    def padding(self, password):
+        """
+        Add padding when it's necessary for AES block before encryptio
+        Length of password must a multiple of 16 (block of 16 bytes) for cipher
+        """
+        print('[*] Length before padding: ',str(len(password)))
+        padd_added = 0
+        if type(password) != bytes :
+            password = password.encode('utf-8')
+        while len(password) % BLOCK_SIZE != 0 :
+            password += PADD_CHAR
+            padd_added += 1
+        print('[+] Password after padding: ', password) 
+        print('[+] Length after padding: ',str(len(password)))
+        return password
+
+    def unpadding(self, password):
+        """
+        Unpadding the characters added by padding function after decryption
+        """
+        password = password.decode('utf-8')
+        while password[-1] == PADD_CHAR.decode('utf-8'):
+            password = password[:-1]
+        return password
+
+
+
     def encrypt(self, plaintext):
+        """
+        Encrypt plaintext to ciphertext
+        """
+        plaintext = self.padding(plaintext)
         self.cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        plaintext = bytes(plaintext,'utf-8')
-        plaintext_padded = pad(plaintext, BLOCK_SIZE)
-        cipher_text = self.cipher.encrypt(plaintext_padded)
-        print('cipher text hex:',  hexa(cipher_text).decode())
-        print('cipher text bytes:', cipher_text)
+        cipher_text = self.cipher.encrypt(plaintext)
+        print('[+] Encryption plain text :', cipher_text)
         return cipher_text
 
+
     def decrypt(self, ciphertext):
+        """
+        Decrypt ciphertext to plaintext
+        """
         self.cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        ciphertext_unpadded = unpad(ciphertext, BLOCK_SIZE)
-        print(ciphertext_unpadded)
-        plaintext = self.cipher.decrypt(ciphertext_unpadded)
-        return hexa(plaintext).decode()
+        plaintext = self.cipher.decrypt(ciphertext)
+        plaintext = self.unpadding(plaintext)
+        print('[+] Decryption cipher text :', plaintext)
+        return plaintext
 
+    
 
-c = AESCipher('AES',16)
--- src/TestCipher.py
+++ src/TestCipher.py
@@ -1,24 +1,20 @@
 from Cipher import AESCipher
 
-nb_test = 4
+nb_test = 2
 score = 0
 
 
-def init_cipher(type='AES', size=32):
-    return True, AESCipher(type, size)
-
-def test_decrypt(self, c, message ):
-    pass
+def init_cipher():
+    return True, AESCipher()
 
 
-
-def test_encrypt( c , message): 
+def test_cipher( c , message): 
     ciphertext = c.encrypt(message)
     plaintext = c.decrypt(ciphertext)
+    if plaintext == message :
+        return True
+    return False
     
-    
-    
-
 
 def get_score(result, score, nb_test):
     if result :
@@ -29,16 +25,17 @@ def get_score(result, score, nb_test):
         print('[!] Test not passed')
         print('[-] Score: '+str(score)+'/'+str(nb_test))
 
-    
+    return score
 
-def main_test(type, size, score, nb_test):
-    result = init_cipher(type, size)
-    get_score(result[0], score, nb_test)
+def main_test(score, nb_test):
+    result = init_cipher()
+    score = get_score(result[0], score, nb_test)
     cipher = result[1]
-    test_encrypt(cipher, '12345kqosfù)$/;s@{é&g')
+    result2 = test_cipher(cipher, '12345kqosfù)$/;s@{é&g')
+    score = get_score(result2, score, nb_test)
 
 
 
-main_test('AES', 32, score, nb_test)
+main_test(score, nb_test)
 
     
